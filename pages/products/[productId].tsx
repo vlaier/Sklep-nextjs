8d456d5cbb794436e6c09b02b5accbf135ac5b68
vlaier <piotrzielu.1996@gmail.com>
2023-01-07 18:49:56 +0100

    Change API server

--- pages/products/[productId].tsx
+++ pages/products/[productId].tsx
@@ -33,7 +33,7 @@ export const getStaticProps = async ({
     };
   }
   const res = await fetch(
-    `https://fakestoreapi.com/products/${params?.productId}`
+    `https://naszsklep-api.vercel.app/api/products${params?.productId}`
   );
   const data: StoreApiResponse | null = await res.json();
   return {
@@ -58,7 +58,7 @@ interface StoreApiResponse {
 export default ProductIdPage;
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://fakestoreapi.com/products/`);
+  const res = await fetch(`https://naszsklep-api.vercel.app/api/products`);
   const data: StoreApiResponse[] = await res.json();
   return {
     paths: data.map((product) => {
